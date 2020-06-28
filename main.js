12ee1c8f053b9d7d5a9b7446e83a00c385601403
Jalissa Williams <jalissa_williams@yahoo.com>
2020-06-28 15:20:29 -0400

    added more styles

--- main.js
+++ main.js
@@ -4,42 +4,40 @@ const logo = document.querySelector('.logo');
 const podcastSect = document.querySelector('.podcast')
 const menu = document.querySelector('.shownav')
 const toggler = document.querySelector('.toggle')
- 
-
-
+const hamburger = document.querySelectorAll('.menuline')
+const body = document.querySelector('body');
 
 
 //Change Color of background when you hover on links
-for(var i = 0; i < links.length; i++) {
-    links[i].addEventListener('mouseover', function (){
-        const colors = ['purple', 'pink', 'hotpink', 'lightpurple', 
-                        'plum', 'palevioletred', 'lightcoral', 'fuchsia' ]
-            colors.forEach(color => {
-                video.style.background = colors[Math.floor(Math.random() * colors.length)]; 
-                video.style.opacity = 0.4;
-                video.style.transition = 'ease-in 0.2s';
-            });
+for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener('mouseover', function () {
+        const colors = ['purple', 'pink', 'hotpink', 'lightpurple',
+            'plum', 'palevioletred', 'lightcoral', 'fuchsia']
+        colors.forEach(color => {
+            video.style.background = colors[Math.floor(Math.random() * colors.length)];
+            video.style.opacity = 0.4;
+            video.style.transition = 'ease-in 0.2s';
         });
-    }
+    });
+}
 
-for(var i = 0; i < links.length; i++) {
-    links[i].addEventListener('mouseout', function (){
+for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener('mouseout', function () {
         const colors = ['']
-            colors.forEach(color => {
-                video.style.background = 'white'; 
-            });
-        
+        colors.forEach(color => {
+            video.style.background = 'white';
+        });
+
     })
 }
 
 // Nav comes in on scroll
-window.addEventListener('scroll', function() {
-    console.log('scrolled')
+window.addEventListener('scroll', function () {
     const nav = document.querySelector('.nav');
     const screenPosition = podcastSect.getBoundingClientRect().top;
-    const position = window.innerHeight / 7 ;
-    
-    
+    const position = window.innerHeight / 7;
+
+
     if (screenPosition < position) {
         nav.style.background = 'black';
         logo.style.background = 'rgba(230, 10, 120, 0.6)';
@@ -49,8 +47,22 @@ window.addEventListener('scroll', function() {
     }
 });
 
-
-toggler.addEventListener('click', function(){
+//Have menu come in
+toggler.addEventListener('click', function () {
     menu.classList.toggle('appear');
-    console.log('clicked')
+    for (var i = 0; i < hamburger.length; i++) {
+        hamburger[i].classList.toggle('changeColor');
+    }
 })
+
+//Change color of hamburger 
+
+for (var i = 0; i < hamburger.length; i++) {
+    // hamburger[i].style.background = 'white';
+}
+
+// let myAnimation = anime({
+//     targets: 'main',
+//     translateX: 500,
+//     borderRadius: 50
+// })
