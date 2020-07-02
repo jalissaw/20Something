42e490c09697b84ea9283d1c254ff8916ddcc065
Jalissa Williams <jalissa_williams@yahoo.com>
2020-07-01 21:21:04 -0400

    added json file

--- ajax.js
+++ ajax.js
@@ -1,31 +1,44 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
+const arrowLeft = document.querySelector('.left');
+const arrowRight = document.querySelector('.right');
+const chevronLeft = document.querySelector('.fa-chevron-left')
+const chevronRight = document.querySelector('.fa-chevron-right')
+const epiNum = document.querySelector('.episodenum');
+const epiTitle = document.querySelector('.episodetitle');
+const epiDescription = document.querySelector('.episodedescription')
+
 
 const xhr = new XMLHttpRequest();
 
 xhr.onload = function () {
-    console.log(xhr.responseText)
+    const episodes = JSON.parse(this.responseText);
+    let i = 1
+    arrowRight.addEventListener('click', () => {
+        function episodeNumber() {
+            if (i < episodes.length) {
+                epiNum.innerHTML = `Episode ${episodes[i].episode}`
+                epiTitle.innerHTML = `${episodes[i].title}`;
+                epiDescription.innerHTML = `${episodes[i].description}`;
+                i++
+                console.log(i)
+                console.log(episodes[i].episode)
+                console.log(episodes[i].title)
+            } else episodeNumberLeft()
+
+
+        }
+        episodeNumber()
+    })
+    arrowLeft.addEventListener('click', () => {
+        function episodeNumberLeft() {
+            console.log('clicked')
+
+
+        }
+        episodeNumberLeft()
+    })
 }
 
-xhr.open("GET", 'https://api.soundcloud.com/tracks?client_id=Spgf2MUV4o55Tj3QFY6XWyhwZ5Cf277X', true);
-=======
-
-const xhr = new XMLHttpRequest();
-
-xhr.onload = function () {
-    console.log(xhr.responseText)
-}
-
-xhr.open("GET", 'https://api.soundcloud.com/tracks?client_id=Spgf2MUV4o55Tj3QFY6XWyhwZ5Cf277X', true);
->>>>>>> 78173b55b7b99a506b58e5440ec860613e6c3543
-=======
-
-const xhr = new XMLHttpRequest();
-
-xhr.onload = function () {
-    console.log(xhr.responseText)
-}
-
-xhr.open("GET", 'https://api.soundcloud.com/tracks?client_id=Spgf2MUV4o55Tj3QFY6XWyhwZ5Cf277X', true);
->>>>>>> 78173b55b7b99a506b58e5440ec860613e6c3543
+xhr.open('GET', 'data/episodes.json', true);
+xhr.setRequestHeader("content-type", "application/json");
 xhr.send();
