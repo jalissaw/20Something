3bcb71aade01b2fcb538aabf8b050a464746247a
Jalissa Williams <jalissa_williams@yahoo.com>
2020-07-08 21:53:13 -0400

    added to podcast

--- main.js
+++ main.js
@@ -6,60 +6,77 @@ const menu = document.querySelector('.shownav')
 const toggler = document.querySelector('.toggle')
 const hamburger = document.querySelectorAll('.menuline')
 const body = document.querySelector('body');
-
+const words = document.querySelectorAll('.bounce p');
+const bounceDiv = document.querySelector('.bounce');
 
 //Change Color of background when you hover on links
-for (var i = 0; i < links.length; i++) {
-    links[i].addEventListener('mouseover', function () {
-        const colors = ['purple', 'pink', 'hotpink', 'lightpurple',
-            'plum', 'palevioletred', 'lightcoral', 'fuchsia']
-        colors.forEach(color => {
-            video.style.background = colors[Math.floor(Math.random() * colors.length)];
-            video.style.opacity = 0.4;
-            video.style.transition = 'ease-in 0.2s';
-        });
-    });
-}
+// for (var i = 0; i < links.length; i++) {
+//     links[i].addEventListener('mouseover', function () {
+//         const colors = ['purple', 'pink', 'hotpink', 'lightpurple',
+//             'plum', 'palevioletred', 'lightcoral', 'fuchsia']
+//         colors.forEach(color => {
+//             video.style.background = colors[Math.floor(Math.random() * colors.length)];
+//             video.style.opacity = 0.4;
+//             video.style.transition = 'ease-in 0.2s';
+//         });
+//     });
+// }
 
-for (var i = 0; i < links.length; i++) {
-    links[i].addEventListener('mouseout', function () {
-        const colors = ['']
-        colors.forEach(color => {
-            video.style.background = 'white';
-        });
+// for (var i = 0; i < links.length; i++) {
+//     links[i].addEventListener('mouseout', function () {
+//         const colors = ['']
+//         colors.forEach(color => {
+//             video.style.background = 'white';
+//         });
 
-    })
-}
+//     })
+// }
 
 // Nav comes in on scroll
-window.addEventListener('scroll', function () {
-    const nav = document.querySelector('.nav');
-    const screenPosition = podcastSect.getBoundingClientRect().top;
-    const position = window.innerHeight / 7;
+// window.addEventListener('scroll', function () {
+//     const nav = document.querySelector('.nav');
+//     const screenPosition = podcastSect.getBoundingClientRect().top;
+//     const position = window.innerHeight / 7;
 
 
-    if (screenPosition < position) {
-        nav.style.background = 'rgb(31, 31, 31)';
-        logo.style.background = 'rgba(230, 10, 120, 0.6)';
-    } else {
-        nav.style.background = 'transparent';
-        logo.style.background = 'transparent';
-    }
-});
+//     if (screenPosition < position) {
+//         nav.style.background = 'rgb(31, 31, 31)';
+//         logo.style.background = 'rgba(230, 10, 120, 0.6)';
+//     } else {
+//         nav.style.background = 'transparent';
+//         logo.style.background = 'transparent';
+//     }
+// });
 
 //Have menu come in
-toggler.addEventListener('click', function () {
-    menu.classList.toggle('appear');
-    for (var i = 0; i < hamburger.length; i++) {
-        hamburger[i].classList.toggle('changeColor');
-    }
-})
+// toggler.addEventListener('click', function () {
+//     menu.classList.toggle('appear');
+//     for (var i = 0; i < hamburger.length; i++) {
+//         hamburger[i].classList.toggle('changeColor');
+//     }
+// })
 
 //Change color of hamburger 
 
 for (var i = 0; i < hamburger.length; i++) {
     // hamburger[i].style.background = 'white';
 }
+
+//Bounce words in podcast tab
+
+
+for (let i = 0; i < words.length; i++) {
+    setInterval(function timer() {
+
+        let width = Math.floor(Math.random() * innerWidth - 100) + 1;
+        let height = Math.floor(Math.random() * innerHeight - 100) + 1;
+        words[i].style.transform = `translate(${width}px, ${height}px) `;
+        words[i].style.transition = '1s ease-in-out'
+    }, i * 2000);
+}
+// setInterval(bounceWords, 1000)
+// bounceWords()
+
 
 
 // let myAnimation = anime({
