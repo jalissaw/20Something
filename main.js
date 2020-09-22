ab5591d28e59aaec1bfc46a994e723aa2e810335
Jalissa Williams <jalissa_williams@yahoo.com>
2020-09-21 23:42:59 -0400

    refactoring and changing styles

--- main.js
+++ main.js
@@ -10,51 +10,51 @@ const words = document.querySelectorAll('.bounce p');
 const bounceDiv = document.querySelector('.bounce')
 
 //Change Color of background when you hover on links
-// for (var i = 0; i < links.length; i++) {
-//     links[i].addEventListener('mouseover', function () {
-//         const colors = ['purple', 'pink', 'hotpink', 'lightpurple',
-//             'plum', 'palevioletred', 'lightcoral', 'fuchsia']
-//         colors.forEach(color => {
-//             video.style.background = colors[Math.floor(Math.random() * colors.length)];
-//             video.style.opacity = 0.4;
-//             video.style.transition = 'ease-in 0.2s';
-//         });
-//     });
-// }
+for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener('mouseover', function () {
+        const colors = ['purple', 'pink', 'hotpink', 'lightpurple',
+            'plum', 'palevioletred', 'lightcoral', 'fuchsia']
+        colors.forEach(color => {
+            video.style.background = colors[Math.floor(Math.random() * colors.length)];
+            video.style.opacity = 0.4;
+            video.style.transition = 'ease-in 0.2s';
+        });
+    });
+}
 
-// for (var i = 0; i < links.length; i++) {
-//     links[i].addEventListener('mouseout', function () {
-//         const colors = ['']
-//         colors.forEach(color => {
-//             video.style.background = 'white';
-//         });
+for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener('mouseout', function () {
+        const colors = ['']
+        colors.forEach(color => {
+            video.style.background = 'white';
+        });
 
-//     })
-// }
+    })
+}
 
 // Nav comes in on scroll
-// window.addEventListener('scroll', function () {
-//     const nav = document.querySelector('.nav');
-//     const screenPosition = podcastSect.getBoundingClientRect().top;
-//     const position = window.innerHeight / 7;
+window.addEventListener('scroll', function () {
+    const nav = document.querySelector('.nav');
+    const screenPosition = podcastSect.getBoundingClientRect().top;
+    const position = window.innerHeight / 7;
 
 
-//     if (screenPosition < position) {
-//         nav.style.background = 'rgb(31, 31, 31)';
-//         logo.style.background = 'rgba(230, 10, 120, 0.6)';
-//     } else {
-//         nav.style.background = 'transparent';
-//         logo.style.background = 'transparent';
-//     }
-// });
+    if (screenPosition < position) {
+        nav.style.background = 'rgb(31, 31, 31)';
+        logo.style.background = 'rgba(230, 10, 120, 0.6)';
+    } else {
+        nav.style.background = 'transparent';
+        logo.style.background = 'transparent';
+    }
+});
 
 //Have menu come in
-// toggler.addEventListener('click', function () {
-//     menu.classList.toggle('appear');
-//     for (var i = 0; i < hamburger.length; i++) {
-//         hamburger[i].classList.toggle('changeColor');
-//     }
-// })
+toggler.addEventListener('click', function () {
+    menu.classList.toggle('appear');
+    for (var i = 0; i < hamburger.length; i++) {
+        hamburger[i].classList.toggle('changeColor');
+    }
+})
 
 //Change color of hamburger 
 
